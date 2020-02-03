50e0c2bde58bc9fcc81efda7dabdf0a11c00927e
alphabooy <Cl3m48@gmail.com>
2020-02-03 14:56:04 +0100

    fix 1.3.1 persistance Project et Enterprise

--- src/main/java/ourbusinessproject/Enterprise.java
+++ src/main/java/ourbusinessproject/Enterprise.java
@@ -1,40 +1,36 @@
 package ourbusinessproject;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.*;
 
+@Entity
 public class Enterprise {
 
-    @NotNull
-    @NotBlank
-    private String name;
-    @NotNull
-    @NotBlank
-    @Size(min = 10)
-    private String description;
-    @NotNull
-    @NotBlank
-    private String contactname;
-    @NotNull
-    @NotBlank
-    @Email
-    private String contactemail;
+    private @Id @GeneratedValue Long id;
+    private @NotEmpty String name;
+    private @NotEmpty @Size(min = 10) String description;
+    private @NotEmpty String contactname;
+    private @NotEmpty @Email String contactemail;
 
     public void setName(String name) {
         this.name = name;
     }
+    public @NotEmpty String getName() { return this.name; }
 
     public void setDescription(String description) {
         this.description = description;
     }
+    public @NotEmpty String getDescription() { return this.description; }
 
     public void setContactName(String contactname) {
         this.contactname = contactname;
     }
+    public @NotEmpty String getContactName() { return this.contactname; }
 
     public void setContactEmail(String contactemail) {
         this.contactemail = contactemail;
     }
+    public @NotEmpty String getContactEmail() { return this.contactemail; }
 }
